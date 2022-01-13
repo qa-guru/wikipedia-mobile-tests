4bc0372c615c4f34b56d33f8387ee0d038bb62ed
svasenkov <aanher@gmail.com>
2022-01-13 21:40:00 +0300

    added java + appium + selenide sample test

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,30 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import drivers.BrowserstackMobileDriver;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBase {
+    @BeforeAll
+    public static void setup() {
+        Configuration.browser = BrowserstackMobileDriver.class.getName();
+        Configuration.startMaximized = false;
+        Configuration.browserSize = null;
+        Configuration.timeout = 10000;
+    }
+
+    @BeforeEach
+    public void startDriver() {
+        open();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        closeWebDriver();
+    }
+}
